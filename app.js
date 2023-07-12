cc46bd1593edf471014467f8af1df5753d442985
Om Patel <137379875+git-om@users.noreply.github.com>
2023-07-12 22:16:32 +0530

    add comments to app.js

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
-const express = require("express");
-const http = require("https");
-const chapter = require(__dirname+"/chapter.js")
+// Import required modules
+const express = require("express"); // Import the Express module
+const http = require("https"); // Import the HTTP module
+const chapter = require(__dirname + "/chapter.js"); // Import the chapter module
 
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables from .env file
 
-const app = express();
+const app = express(); // Create an Express application
 
-app.use(express.static('public'))
+app.use(express.static('public')); // Serve static files from the 'public' directory
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); // Set the view engine to EJS
 
-app.get("/", function (request, response) {
+app.get("/", function (request, response) { // Define a route handler for the root URL ("/")
 
-  var randomChapterNumber = Math.floor((Math.random() * 18));
-  const currentChapterData = chapterList[randomChapterNumber]
+  // Generate a random chapter number
+  var randomChapterNumber = Math.floor((Math.random() * 18)); 
+
+  // Get the data of the current chapter
+  const currentChapterData = chapterList[randomChapterNumber];
+
+  // Increment the chapter number
   randomChapterNumber++;
+
+  // Get the number of verses in the current chapter
   const verseInChapter = currentChapterData.verses_count;
-  const randomVerseNUmber = Math.floor((Math.random()*verseInChapter)+1);
+
+  // Generate a random verse number within the chapter
+  const randomVerseNUmber = Math.floor((Math.random() * verseInChapter) + 1);
 
   const options = {
     method: "GET",
     hostname: "bhagavad-gita3.p.rapidapi.com",
     port: null,
-    path: "/v2/chapters/"+randomChapterNumber+"/verses/"+randomVerseNUmber+"/",
+    path: "/v2/chapters/" + randomChapterNumber + "/verses/" + randomVerseNUmber + "/",
     headers: {
-      "X-RapidAPI-Key": process.env.GITA_API,
+      "X-RapidAPI-Key": process.env.GITA_API, // Set the RapidAPI Key from environment variables
       "X-RapidAPI-Host": "bhagavad-gita3.p.rapidapi.com",
     },
   };
@@ -41,13 +51,15 @@ app.get("/", function (request, response) {
       body = body.toString();
       data = JSON.parse(body);
 
+      // Extract the relevant data from the response
       verseData = {
         verse: data.text,
         verseNumber: data.slug,
         translation: data.translations[3].description,
         commentaries: data.commentaries[13].description
       }
-      // response.send(data)
+
+      // Render the 'index' view with the verse data
       response.render('index', verseData);
     });
   });
@@ -55,6 +67,6 @@ app.get("/", function (request, response) {
   req.end();
 });
 
-app.listen(3000, function () {
+app.listen(3000, function () { // Start the server and listen on port 3000
   console.log("app running on port 3000.");
 });
